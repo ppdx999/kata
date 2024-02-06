4127af1b06e1579100f0a01113f62db105c85634
ppdx999 <ppdx999@gmail.com>
2024-02-06 09:41:31 +0900

    use static string for cmd name

--- tests/cli.rs
+++ tests/cli.rs
@@ -2,9 +2,11 @@ use assert_cmd::prelude::*; // Add methods on commands
 use predicates::prelude::*; // Used for writing assertions
 use std::process::Command; // Run programs
 
+static CMD_NAME: &'static str = "kata";
+
 #[test]
 fn run_program() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("kata")?; // Create a new command
+    let mut cmd = Command::cargo_bin(CMD_NAME)?; // Create a new command
     cmd.assert().success().stdout(
         predicate::str::contains("Hello, world!") // Check that the command succeeds and that "Hello, world!" is printed to stdout
     );
