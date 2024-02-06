cacb0f3b593b438cdec576221a992518be8e90e1
ppdx999 <ppdx999@gmail.com>
2024-02-06 09:52:39 +0900

    add test for failture case

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     println!("Hello, world!");
+    Err(std::io::Error::new(std::io::ErrorKind::Other, "An error occurred"))
 }
--- tests/cli.rs
+++ tests/cli.rs
@@ -7,8 +7,9 @@ static CMD_NAME: &'static str = "kata";
 #[test]
 fn run_program() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin(CMD_NAME)?; // Create a new command
-    cmd.assert().success().stdout(
+    cmd.assert().failure().stdout(
         predicate::str::contains("Hello, world!") // Check that the command succeeds and that "Hello, world!" is printed to stdout
     );
     Ok(())
 }
+
