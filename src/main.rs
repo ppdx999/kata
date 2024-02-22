403d2f01084ccc8d96595682b5ec1a6b2a906bd1
ppdx999 <ppdx999@gmail.com>
2024-02-22 23:43:23 +0900

    refactoring: Schema should have method to validate text.

--- src/main.rs
+++ src/main.rs
@@ -76,6 +76,56 @@ impl Schema {
 
         Ok(schema)
     }
+
+    fn validate(self: &Schema, line: String) -> Result<(), String> {
+        let values = line.split_whitespace().collect::<Vec<&str>>();
+        if values.len() != self.terms.len() {
+            return Err(format!("Invalid number of values: {}", values.len()));
+        }
+        for (i, value) in values.iter().enumerate() {
+            let term = &self.terms[i];
+            let mut is_valid = false;
+            for type_ in &term.types {
+                match type_ {
+                    Type::Integer => {
+                        if value.parse::<i64>().is_ok() {
+                            is_valid = true;
+                            break;
+                        }
+                    }
+                    Type::Float => {
+                        if value.parse::<f64>().is_ok() {
+                            is_valid = true;
+                            break;
+                        }
+                    }
+                    Type::String => {
+                        is_valid = true;
+                        break;
+                    }
+                    Type::Boolean => {
+                        match value.to_lowercase().as_str() {
+                            "true" | "false" => {
+                                is_valid = true;
+                                break;
+                            }
+                            _ => {}
+                        }
+                    }
+                    Type::Null => {
+                        if *value == "_" {
+                            is_valid = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            if !is_valid {
+                return Err(format!("Invalid value: {}", value));
+            }
+        }
+        Ok(())
+    }
 }
 
 #[test]
@@ -120,59 +170,19 @@ fn test_schema_from_text() {
     assert_eq!(Schema::from_text("id:integer name:string is_active:boolean price:float err:extra").unwrap_err(), "Invalid type: extra");
 }
 
-fn validate(schema: &Schema, line: &str) -> Result<(), String> {
-    let values = line.split_whitespace().collect::<Vec<&str>>();
+#[test]
+fn test_validate() {
+    let schema = Schema::from_text("id:integer name:string is_active:boolean price:float deleted_field:null optional_field:string|null").unwrap();
 
-    if values.len() != schema.terms.len() {
-        return Err(format!("Invalid number of values: {}", values.len()));
-    }
+    // correct values
+    assert_eq!(schema.validate("1 john_doe true 100.0 _ _".to_owned()).unwrap(), ());
+    assert_eq!(schema.validate("1 john_doe true\t100.0 _ _".to_owned()).unwrap(), ());
 
-    for (i, value) in values.iter().enumerate() {
-        let term = &schema.terms[i];
-        let mut is_valid = false;
+    // // multiple whitespaces
+    assert_eq!(schema.validate("1  john_doe  true  100.0  _ _".to_owned()).unwrap(), ());
 
-        for type_ in &term.types {
-            match type_ {
-                Type::Integer => {
-                    if value.parse::<i64>().is_ok() {
-                        is_valid = true;
-                        break;
-                    }
-                }
-                Type::Float => {
-                    if value.parse::<f64>().is_ok() {
-                        is_valid = true;
-                        break;
-                    }
-                }
-                Type::String => {
-                    is_valid = true;
-                    break;
-                }
-                Type::Boolean => {
-                    match value.to_lowercase().as_str() {
-                        "true" | "false" => {
-                            is_valid = true;
-                            break;
-                        }
-                        _ => {}
-                    }
-                }
-                Type::Null => {
-                    if *value == "_" {
-                        is_valid = true;
-                        break;
-                    }
-                }
-            }
-        }
-
-        if !is_valid {
-            return Err(format!("Invalid value: {}", value));
-        }
-    }
-
-    Ok(())
+    // // incorrect values
+    assert_eq!(schema.validate("1 john_doe true 100.0 _ _ extra".to_owned()).unwrap_err(), "Invalid number of values: 7");
 }
 
 fn main() -> Result<(), String> {
@@ -187,24 +197,8 @@ fn main() -> Result<(), String> {
 
     for line in reader.lines() {
         let line = line.unwrap();
-        validate(&schema, line.as_str())?;
+        schema.validate(line)?;
     }
 
     Ok(())
-}
-
-
-#[test]
-fn test_validate() {
-    let schema = Schema::from_text("id:integer name:string is_active:boolean price:float deleted_field:null optional_field:string|null").unwrap();
-
-    // correct values
-    assert_eq!(validate(&schema, "1 john_doe true\t100.0 _ _").unwrap(), ());
-    assert_eq!(validate(&schema, "1 john_doe true\t100.0 _ text").unwrap(), ());
-
-    // multiple whitespaces
-    assert_eq!(validate(&schema, "1  john_doe  true  100.0  _ _").unwrap(), ());
-
-    // incorrect values
-    assert_eq!(validate(&schema, "1 john_doe true 100.0 _ _ extra").unwrap_err(), "Invalid number of values: 7");
 }
