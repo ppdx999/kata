a858f52600032711ed27802d00055986efb72c12
ppdx999 <ppdx999@gmail.com>
2024-03-03 17:44:23 +0900

    #5 write test for --schema-type option

--- tests/cli.rs
+++ tests/cli.rs
@@ -16,3 +16,17 @@ fn run_program() -> Result<(), Box<dyn std::error::Error>> {
     cmd.assert().success();
     Ok(())
 }
+
+#[test]
+fn run_program_with_options() -> Result<(), Box<dyn std::error::Error>> {
+    // arrange
+    let mut cmd = Command::cargo_bin(CMD_NAME)?;
+    cmd.arg("id:integer email:string is_active:boolean name:string|null")
+        .arg("--schema-type")
+        .arg("tsv")
+        .write_stdin("1 jhon_doe@example.com true Jhon_Doe\n2 emily_lua@example.com false Emily_Lua\n3 mac_kily@example.com true _");
+
+    // act & assert
+    cmd.assert().success();
+    Ok(())
+}
