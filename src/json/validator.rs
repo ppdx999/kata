8f9c481772985188abb2964a646e2882ede78884
ppdx999 <ppdx999@gmail.com>
2024-03-12 10:27:38 +0900

    #5 impl validator: simple object

--- src/json/validator.rs
+++ src/json/validator.rs
@@ -1,29 +1,58 @@
 use serde_json;
 use serde_json::{Map, Value};
-use crate::json::data;
+use crate::json::data as schema;
 
 #[allow(dead_code)]
 pub struct Validator;
 
 impl Validator {
-    pub fn validate(node: &data::Value, text: &str) -> Result<bool, String> {
+    pub fn validate(schema: &schema::Value, text: &str) -> Result<bool, String> {
         let value: serde_json::Value = serde_json::from_str(text).map_err( |e| e.to_string() )?;
         match value {
-            Value::Object(object) => Self::object(&node, object),
+            Value::Object(object) => match schema {
+                schema::Value::Object(schema) => Self::object(&schema, &object),
+            },
             _ => Err("Expected an object".to_string()),
         }
     }
 
-    fn object(schema: &data::Value, object: Map<String, Value>) -> Result<bool, String> {
+    fn object(schema: &schema::Object, object: &Map<String, Value>) -> Result<bool, String> {
+        for property in &schema.properties {
+            let value = object.get(&property.name).ok_or(format!("Property '{}' not found", property.name))?;
+            Self::property(&property, value)?;
+        }
         Ok(true)
+    }
+
+    fn property(schema: &schema::Property, value: &Value) -> Result<bool, String> {
+        match &schema.type_ {
+            schema::Type::String => match value {
+                Value::String(_) => Ok(true),
+                _ => Err("Expected a string".to_string()),
+            },
+            schema::Type::Object(schema) => match value {
+                Value::Object(object) => Self::object(schema, object),
+                _ => Err("Expected an object".to_string()),
+            },
+        }
     }
 }
 
 #[test]
-fn test_validator() {
+fn test_empty_object() {
     use crate::json::schema::Schema;
 
     let schema = Schema::from_text("{}").unwrap();
     assert!(Validator::validate(&schema.root, "{}").unwrap());
 }
 
+#[test]
+fn test_simple_object() {
+    use crate::json::schema::Schema;
+
+    let schema = Schema::from_text("{name: string}").unwrap();
+
+    assert!(Validator::validate(&schema.root, r#"{"name": "John"}"#).unwrap());
+
+    assert_eq!(Validator::validate(&schema.root, r#"{"nome": "John"}"#).unwrap_err(), "Property 'name' not found");
+}
